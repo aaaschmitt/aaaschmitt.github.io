7bfa66681d732a7772281cc12703116a040faaf6
Andy Schmitt <aschmitt@berkeley.edu>
2015-07-19 23:58:07 -0700

    testing build again

--- src/test.jsx
+++ src/test.jsx
@@ -2,7 +2,7 @@ define(['jsx!src/main'], function(React) {
 
 	var Random = React.createClass({
 		render: function() {
-			return (<div>Look a seperate file added to the build</div>);
+			return (<div>Look a seperate file added to the build. Added another sentence to see if build is really working</div>);
 		}
 	})
 
