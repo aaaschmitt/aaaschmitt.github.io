a8d8fad9f733b032cd8a8d50907835204b76e030
Andy Schmitt <aschmitt@berkeley.edu>
2015-07-21 00:30:39 -0700

    let's deploy

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ git commit -m "Deploy to GitHub Pages via Travis CI"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${DEPLOY_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${DEPLOY_TOKEN}@${GH_REF}" master > /dev/null 2>&1
