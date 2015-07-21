82894c0763c55b78fbff1fe9687dca447c1bbb6d
Andy Schmitt <aschmitt@berkeley.edu>
2015-07-21 00:25:10 -0700

    come on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 # go to the out directory and create a *new* Git repo
-cd build
+cd dist
 git init
 
 # inside this git repo we'll pretend to be a new user
