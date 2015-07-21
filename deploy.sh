2a1a98fbb8fb352d0c808b5f784c37ab17c405cb
Andy Schmitt <aschmitt@berkeley.edu>
2015-07-21 00:45:01 -0700

    this deply sucks

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ cd dist
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "aschmitt@berkeley.edu"
+git config --global user.name "Andy Schmitt"
+git config --global user.email "aschmitt@berkeley.edu"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages via Travis CI"
+git commit -m "Deploy to aaaschmitt.github.io via Travis CI"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
