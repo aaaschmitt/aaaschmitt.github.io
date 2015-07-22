9e6ac12746a867ca487486b8de553b2d441fcc87
Andy Schmitt <aschmitt@berkeley.edu>
2015-07-21 19:59:26 -0700

    Deploy to aaaschmitt.github.io via Travis CI

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# go to the out directory and create a *new* Git repo
-
+./build.sh
 # inside this git repo we'll pretend to be a new user
 git config --global user.name "Andy Schmitt"
 git config --global user.email "aschmitt@berkeley.edu"
