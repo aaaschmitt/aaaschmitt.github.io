05b41e5789e20e7ac85330b60f1622cef698ddf2
Travis CI <aschmitt@berkeley.edu>
2015-07-21 22:48:55 -0700

    hiding the token. deleted the old ones from github

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,4 @@ git commit -m "Deploy via Travis CI at: ${time}"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-echo  "https://${DEPLOY_TOKEN}@${GH_REF}"
 git push --force "https://${DEPLOY_TOKEN}@${GH_REF}" master > /dev/null 2>&1
