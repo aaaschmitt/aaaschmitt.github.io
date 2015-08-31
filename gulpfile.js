c6a667bd40b97761552ae2425e47d7c8f19eb79c
Travis CI <aschmitt@berkeley.edu>
2015-08-31 10:39:12 -0700

    [ci skip] spaceship animated. new build completed

--- gulpfile.js
+++ gulpfile.js
@@ -10,30 +10,76 @@ var gulp = require('gulp'),
     del = require('del'),
     runseq = require('run-sequence');
 
-var main_components = ['bower_components/react/react.js', 'bower_components/jquery/dist/jquery.min.js', 'bower_components/three.js/build/three.min.js', 'src/main-jsx/**.jsx'],
-    css_files = ['src/css/**.less', 'src/css/**.css'],
-    assets = ['assets/*'],
-    jekyll_config = ['jekyll/Gemfile', 'jekyll/Gemfile.lock', 'jekyll/config.yml'],
-    jekyll_folders = ['jekyll/_includes/*', 'jekyll/_posts/*', 'jekyll/_layouts/*', 'jekyll/Resume/*', 'jekyll/DataViz/*', 'jekyll/Guides/*'];
+    //these go into every page, need a dev version and a production build version
+var main_js_dev = ['bower_components/react/react.js', 'bower_components/jquery/dist/jquery.js', 
+                  'src/main-jsx/**.jsx'],
+    main_files_build = ['bower_components/react/react.min.js', 'bower_components/jquery/dist/jquery.min.js', 
+                  'src/main-jsx/**.jsx', 'src/main-css/**.css'],
+    main_less = [ 'src/main-less/**.css', 'src/main-less/**.less'],
+
+    //assets just get moved to a new folder in the build out
+    assets = ['src/assets/*'],
+
+    //need to copy over jekyll files and folders
+    jekyll_config = ['src/jekyll/Gemfile', 'src/jekyll/Gemfile.lock', 'src/jekyll/_config.yml'],
+    jekyll_html = ['src/jekyll/_includes/**.html', 'src/jekyll/_posts/**.html', 'src/jekyll/_layouts/**.html', 
+                      'src/jekyll/Resume/**.html', 'src/jekyll/DataViz/**.html', 'src/jekyll/Guides/**.html'];
 
 var path = {
   HOME: 'index.html',
   README: 'README.md',
-  MAIN: main_components,
-  CSS: css_files,
   ASSETS: assets,
   JEKYLL_C: jekyll_config,
-  JEKYLL_F: jekyll_folders,
+  JEKYLL_HTML: jekyll_html,
 
-  ALL: ['index.html'].concat(main_components, css_files, assets, jekyll_config, jekyll_folders),
-
-  JS_MAIN_MINIFIED_OUT: 'build.min.js',
-  CSS_MINIFIED_OUT: 'build.min.css',
+  JS_OUT: 'build.min.js',
+  CSS_OUT: 'build.min.css',
 
   DEST_SRC: 'dist/src',
   DEST_BUILD: 'dist/build',
   DEST: 'dist'
 };
+
+//keep track of what files need to go in each page
+var pageFileMap = {
+    'Home': { 
+            js: ['src/jekyll/Home/jsx/**.jsx'], 
+            less: ['src/jekyll/Home/less/**.less', 'src/jekyll/Home/less/**.css'], 
+            PAGE_OUT: path.DEST
+        },
+    'Resume': { 
+                js: ['bower_components/three.js/build/three.min.js', 'src/jekyll/Resume/jsx/**.jsx'], 
+                less: ['src/jekyll/Resume/less/**.less', 'src/jekyll/Resume/less/**.css'], 
+                PAGE_OUT: path.DEST + '/Resume'
+            },
+    'DataViz': { 
+                js: [], 
+                less: [],
+                PAGE_OUT: path.DEST + '/DataViz'
+            },
+    'Guides': { 
+                js: [], 
+                less: [], 
+                PAGE_OUT: path.DEST + '/Guides'
+            }
+}
+
+function getAllOtherFiles(pageFileMap) {
+    var result = [];
+    for (var pageName in pageFileMap) {
+        if (pageFileMap.hasOwnProperty(pageName)) {
+            var pageProps = pageFileMap[pageName];
+            result.push.apply(result, pageProps.js); 
+            result.push.apply(result, pageProps.less);  
+        }
+    }
+    return result;
+}
+
+//Figure out all of the files to watch
+path.ALL = ['index.html'].concat(main_js_dev, main_less, jekyll_config, jekyll_html, assets, getAllOtherFiles(pageFileMap));
+
+console.log(path.ALL);
 
 /*************
  * DEV BUILD *
@@ -46,41 +92,48 @@ gulp.task('clean', function(cb) {
   ], cb);
 });
 
-//compile .jsx to .js
-//compile .less to .css
-gulp.task('compile', function(){
-  gulp.src(path.MAIN)
-    .pipe(react())
-    .pipe(rename(function(path) {
-    	path.extname = ".js"
-    }))
-    .pipe(gulp.dest(path.DEST_SRC + '/main-js'));
-
-  gulp.src(path.CSS)
-    .pipe(less())
-    .pipe(rename(function(path) {
-      path.extname = ".css"
-    }))
-    .pipe(gulp.dest(path.DEST_SRC + '/css'));
-});
-
 //copy html, jekyll, pure js, and assets to dist
 gulp.task('copy', function(){
   gulp.src(path.HOME)
     .pipe(gulp.dest(path.DEST));
   gulp.src(path.JEKYLL_C)
     .pipe(gulp.dest(path.DEST));
-  gulp.src(path.JEKYLL_F, {base: 'jekyll'})
+  gulp.src(path.JEKYLL_HTML, {base: 'src/jekyll'})
     .pipe(gulp.dest(path.DEST))
   gulp.src(path.ASSETS)
     .pipe(gulp.dest(path.DEST + '/assets'));
 });
 
+//compile .jsx to .js
+//compile .less to .css
+//do it for each file
+gulp.task('compile-dev', function() {
+    for (var pageName in pageFileMap) {
+        if (pageFileMap.hasOwnProperty(pageName)) {
+            var pageProps = pageFileMap[pageName];
+
+            gulp.src(main_js_dev.concat(pageProps.js))
+                .pipe(react())
+                .pipe(order([
+                  'bower_components/**/*.js',
+                  '**/**.jsx'
+                ], {base: '.'}))
+                .pipe(concat(path.JS_OUT))
+                .pipe(gulp.dest(pageProps.PAGE_OUT));
+
+            gulp.src(pageProps.less.concat(main_less))
+                .pipe(less())
+                .pipe(concat(path.CSS_OUT))
+                .pipe(gulp.dest(pageProps.PAGE_OUT));
+        }
+    }
+});
+
 //watch all my files for changes for dev build
 gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['compile', 'copy']);
+  gulp.watch(path.ALL, ['copy', 'compile-dev']);
 });
-gulp.task('default', function(cb) { runseq('clean', ['compile', 'copy', 'watch'], cb) });
+gulp.task('default', function(cb) { runseq('clean', [ 'copy', 'compile-dev', 'watch'], cb) });
 
 
 /********************
--- src/main-jsx/main.jsx
+++ src/main-jsx/main.jsx
@@ -2,6 +2,16 @@
  * Main javascript file for the webiste
  */
 
+function getCenterXCoordOfElement(element) {
+    var offset = element.offset(),
+        width = element.width(),
+        height = element.height();
+
+    var centerX = offset.left + width / 2;
+
+    return centerX;
+}
+
 /** 
  * Renders the top-nav 
  */
@@ -18,13 +28,17 @@ function goToPath(path) {
 }
 var TopNav = React.createClass({
         render: function () {
-            var navContent = [];
+            var navContent = [],
+                name = "";
 
-            navContent.push(<div className="nav-element-first" key="0">{navItems[0].name}</div>);
+            name = navItems[0].name;
+            navContent.push(<div className="nav-element-first" data-navname={name} key="0">{name}</div>);
             for (var i = 1; i < navItems.length-1; i++) {
-                navContent.push(<div className='nav-element' key={i}>{navItems[i].name}</div>);
+                name = navItems[i].name;
+                navContent.push(<div className='nav-element' data-navname={name} key={i}>{name}</div>);
             }
-            navContent.push(<div className='nav-element-last' key={i}>{navItems[i].name}</div>);
+            name = navItems[i].name;
+            navContent.push(<div className='nav-element-last' data-navname={name} key={i}>{name}</div>);
 
             return (
                 <div>{navContent}</div>
@@ -32,12 +46,25 @@ var TopNav = React.createClass({
         },
 
         componentDidMount: function() {
-            console.log("adding handlers");
+            $("*[data-navname='" + pageName + "']").css('color', '#FFED00');
             $("*[class^='nav-element']").each(function(i) {
                 $(this).on('click', function() {
                     goToPath(navItems[i].path);
                 })
             })
+
+            //add spaceship movement handlers if it exists
+            var spaceship = $('#spaceship-img');
+            if (spaceship.length) {
+
+                $("*[class^='nav-element']").on('mouseenter', function() {
+                    var centerX = getCenterXCoordOfElement($(this));
+                    spaceship.animate({'left': (centerX - ($(window).width() / 2)) + "px"}, 1000/60);
+                });
+                $("*[class^='nav-element']").on('mouseleave', function() {
+                    spaceship.animate({'left': 0 + "px"}, 1000/60);
+                });
+            }
         }
     });
 
